f7b8443d77c7a8f8759086a1a80657fb41675dc0
Markus Örebrand <markus.orebrand@gmail.com>
2019-09-13 21:42:07 +0200

    Utkast till lab 1

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # opencv-lab
 
+## Lab 1: Hello Camera
+
+**Labbfil**: lab1_hello_camera.py
+
+    pipenv shell
+    pipenv install
+    python3 lab1_hello_camera.py
+
+1. I vissa operativsystem behöver man godkänna access till kameran
+2. Ett nytt fönster visar en videoström av kameran
+3. Tryck på `q` för att avsluta programmet
+
+## Lab 2: Bildmanipulation
+
+TBD.
---
+++ lab1_hello_camera.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+
+capture = cv2.VideoCapture(0)
+cv2.namedWindow('Webcam', cv2.WINDOW_NORMAL)
+
+while capture.isOpened():
+    ret, frame = capture.read()
+    if ret == True:
+        cv2.imshow('Webcam', frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+capture.release()
+cv2.destroyAllWindows()
